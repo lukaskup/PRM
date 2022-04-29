90f371153e91d56daa52d1c23ebf80a145b45785
lukaskup <lukasz.skup1999@gmail.com>
2022-04-29 10:18:27 +0200

    TaskRepository readAllData and addTask functions

--- app/src/main/java/com/example/myapplication/data/TaskRepository.kt
+++ app/src/main/java/com/example/myapplication/data/TaskRepository.kt
@@ -1,4 +1,12 @@
 package com.example.myapplication.data
 
-class TaskRepository {
+import androidx.lifecycle.LiveData
+
+class TaskRepository(private val taskDao: TaskDao) {
+
+    val readAllData: LiveData<List<Task>> = taskDao.readAllData();
+
+    suspend fun addTask(task: Task){
+        taskDao.addTask(task);
+    }
 }
