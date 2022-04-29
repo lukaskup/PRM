a6de429a8e0ab111d05633c12a54781edd4c2d3f
lukaskup <lukasz.skup1999@gmail.com>
2022-04-29 12:06:30 +0200

    TaskViewModel init

---
+++ app/src/main/java/com/example/myapplication/data/TaskViewModel.kt
@@ -0,0 +1,25 @@
+package com.example.myapplication.data
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class TaskViewModel(application: Application): AndroidViewModel(application) {
+    private val repository: TaskRepository
+    private val readAllData: LiveData<List<Task>>
+
+    init {
+        val taskDao = TaskDatabase.getDatabase(application).taskDao()
+        repository = TaskRepository(taskDao)
+        readAllData = repository.readAllData
+    }
+
+    fun addTask(task: Task){
+        viewModelScope.launch(Dispatchers.IO) {
+            repository.addTask(task)
+        }
+    }
+}
