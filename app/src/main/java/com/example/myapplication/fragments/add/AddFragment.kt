4eb6f7f9b270ff37c92f92f87fef84131c3760ec
lukaskup <lukasz.skup1999@gmail.com>
2022-05-06 23:36:25 +0200

    navController pop route fix

--- app/src/main/java/com/example/myapplication/fragments/add/AddFragment.kt
+++ app/src/main/java/com/example/myapplication/fragments/add/AddFragment.kt
@@ -1,11 +1,15 @@
 package com.example.myapplication.fragments.add
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.OnBackPressedCallback
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
 import com.example.myapplication.R
+
 
 class AddFragment : Fragment() {
     override fun onCreateView(
@@ -13,6 +17,7 @@ class AddFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add, container, false)
+        val view = inflater.inflate(R.layout.fragment_add, container, false)
+        return view
     }
 }
