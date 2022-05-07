af8c494cb16a3dabb27a76255c00bd078e7f7fb0
lukaskup <lukasz.skup1999@gmail.com>
2022-05-07 23:07:39 +0200

    ListAdapter fix

--- app/src/main/java/com/example/myapplication/fragments/list/ListAdapter.kt
+++ app/src/main/java/com/example/myapplication/fragments/list/ListAdapter.kt
@@ -29,20 +29,11 @@ class ListAdapter(private val mTaskViewModel: TaskViewModel): RecyclerView.Adapt
         val currentTask = taskList[position]
         holder.itemView.findViewById<TextView>(R.id.taskTitle_txt).text = currentTask.title.toString()
         holder.itemView.findViewById<TextView>(R.id.taskPriority_txt).text = "Priority: ${currentTask.priority.toString()}"
-        holder.itemView.findViewById<TextView>(R.id.taskProgress_txt).text = "${currentTask.percentageDone.toString()}%"
+        holder.itemView.findViewById<TextView>(R.id.taskProgress_txt).text = "${currentTask.progress.toString()}%"
 
         val myFormat = "dd/MM/yy"
         val dateFormat = SimpleDateFormat(myFormat, Locale.US)
         holder.itemView.findViewById<TextView>(R.id.taskDeadline_txt).text = "Due: ${dateFormat.format(currentTask.deadline)}"
-        holder.itemView.findViewById<ConstraintLayout>(R.id.tasksList).setOnClickListener {
-            val action = ListFragmentDirections.actionListFragmentToDetailsFragment(currentTask)
-            holder.itemView.findNavController().navigate(action)
-        }
-        holder.itemView.findViewById<ConstraintLayout>(R.id.cl_tasksList).setOnLongClickListener() { _ ->
-            deleteTask(currentTask, holder.itemView)
-        }
-
-
     }
 
     fun setData(tasks: List<Task>) {
@@ -52,18 +43,5 @@ class ListAdapter(private val mTaskViewModel: TaskViewModel): RecyclerView.Adapt
 
     override fun getItemCount(): Int {
         return taskList.size
-    }
-
-    private fun deleteTask(task: Task, itemView: View): Boolean {
-        val builder = AlertDialog.Builder(itemView.context)
-        builder.setPositiveButton("Yes") { _, _ ->
-            mTaskViewModel.deleteTask(task)
-            Toast.makeText(itemView.context, "Successfully completed!", Toast.LENGTH_SHORT).show()
-        }
-        builder.setNegativeButton("No") { _, _ -> }
-        builder.setTitle("Complete task")
-        builder.setMessage("Are you sure you want to complete this task?")
-        builder.create().show()
-        return true
     }
 }
